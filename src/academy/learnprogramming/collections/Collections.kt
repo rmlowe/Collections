fc4dcdd64c64f27f8e7f5334bba15da9a6cf60dd
Robert Lowe <robertmlowe@rmlowe.com>
2018-02-23 14:11:41 +0800

    Initial commit

---
+++ src/academy/learnprogramming/collections/Collections.kt
@@ -0,0 +1,8 @@
+package academy.learnprogramming.collections
+
+
+fun main(args: Array<String>) {
+
+    val strings = listOf("spring", "summer", "fall", "winter")
+    println(strings.javaClass)
+}
